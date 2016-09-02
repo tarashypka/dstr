2c98a0c1b12c45e7e60634509e1874dd65f81969
Taras Shypka <tarashypka@gmail.com>
2016-09-02 20:31:11 +0300

    Fixed CustomMongoJndiFactory and configuration for Mongo connection pool to work with replica set

--- src/main/java/com/deoxys/dev/dstr/persistence/CustomMongoJndiFactory.java
+++ src/main/java/com/deoxys/dev/dstr/persistence/CustomMongoJndiFactory.java
@@ -6,14 +6,7 @@ import com.mongodb.ServerAddress;
 
 import javax.naming.*;
 import javax.naming.spi.ObjectFactory;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.Hashtable;
-import java.util.List;
-
-/**
- * Created by deoxys on 08.07.16.
- */
+import java.util.*;
 
 public class CustomMongoJndiFactory implements ObjectFactory {
 
@@ -25,41 +18,56 @@ public class CustomMongoJndiFactory implements ObjectFactory {
 
         validateProperty(obj, "Invalid JNDI object reference");
 
+        Reference ref = (Reference) obj;
+        Enumeration<RefAddr> addresses = ref.getAll();
+
         String dbName = null;
-        String host = null;
+        String host = "localhost";      // default host
         String username = null;
         String password = null;
-        int port = 27017;
+        int port = 27017;               // default port
 
-        Reference ref = (Reference) obj;
-        Enumeration<RefAddr> props = ref.getAll();
-        while (props.hasMoreElements()) {
-            RefAddr addr = props.nextElement();
+        Map<String, String> props = new HashMap<>();
+        while (addresses.hasMoreElements()) {
+            RefAddr addr = addresses.nextElement();
             String propName = addr.getType();
-            String propValue = (String) addr.getContent();
+            String propValue =  (String) addr.getContent();
+
             if (propName.equals("dbName")) {
                 dbName = propValue;
                 validateProperty(dbName, "Invalid or empty mongo database name");
-            } else if (propName.equals("host")) {
-                host = propValue;
-                validateProperty(host, "Invalid or empty mongo host");
             } else if (propName.equals("username")) {
                 username = propValue;
                 validateProperty(username, "Invalid or empty mongo username");
             } else if (propName.equals("password")) {
                 password = propValue;
                 validateProperty(password, "Invalid or empty mongo password");
-            } else if (name.equals("port")) {
-                try {
-                    port = Integer.parseInt(propValue);
-                } catch (NumberFormatException e) {
-                    throw new NamingException("Invalid port value " + propValue);
-                }
-            }
+            } else if (propName.equals("host")) {
+                host = propValue;
+                validateProperty(host, "Invalid or empty mongo host");
+            } else if (propName.equals("port"))
+                port = Integer.parseInt(propValue);
+            else props.put(propName, propValue);
         }
 
+        if (username == null) throw new NameNotFoundException("username");
+        else if (password == null) throw new NameNotFoundException("password");
+        else if (dbName == null) throw new NameNotFoundException("dbName");
+
         List<ServerAddress> seeds = new ArrayList<>();
-        seeds.add(new ServerAddress(host, port));
+
+        // Host, port values if set, than with names "host_1", "port_1", ...
+        String host_, port_;
+        for (int i = 1; ; i++) {
+            if ((host_ = props.get("host_" + i)) != null && ! host_.trim().equals("")) {
+                if ((port_ = props.get("port_" + i)) != null)
+                    seeds.add(new ServerAddress(host_, Integer.parseInt(port_)));
+                else throw new NameNotFoundException("port_" + i);
+            } else break;
+        }
+
+        // Add default host and port if there are no
+        if (seeds.isEmpty()) seeds.add(new ServerAddress(host , port));
 
         List<MongoCredential> auths = new ArrayList<>();
         auths.add(MongoCredential.createCredential(username, dbName, password.toCharArray()));
